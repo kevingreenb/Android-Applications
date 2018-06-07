67ffed1250fd24fae3c7706e7246d1d0f5cd98c1
KGreen <kevin.e.green@hotmail.com>
2018-06-06 22:05:03 -0500

    adding more than just one question type

--- app/src/main/java/com/example/android/quiz/Question.java
+++ app/src/main/java/com/example/android/quiz/Question.java
@@ -2,20 +2,27 @@ package com.example.android.quiz;
 
 /**
  * Created by KevinGreen on 2/8/18.
+ * Credit to Tiberius for help and code.
  */
 import java.util.HashMap;
 import java.util.Map;
 
+// Define the question Types
+enum typeOfQuestion {
+    BUTTON, RADIO, CHECKBOX, TEXTENTRY
+}
+
 public class Question {
     public Map questionSet = new HashMap<>();
 
-    public Question(String questionText,  String answer1, String answer2, String answer3, String answer4, String correctAnswer){
+    public Question(String questionText,  String answer1, String answer2, String answer3, String answer4, String correctAnswer, typeOfQuestion type){
         questionSet.put("question", questionText);
         questionSet.put("a", answer1);
         questionSet.put("b", answer2);
         questionSet.put("c", answer3);
         questionSet.put("d", answer4);
         questionSet.put("answer", correctAnswer);
+        questionSet.put("format", type);
 
 
 
